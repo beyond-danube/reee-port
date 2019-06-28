dcecf932432e75092cf6212b5b1876faa8900f05
Alexey Zadunaiskiy <alexey.zdn@gmail.com>
2019-06-28 18:41:45 +0300

    xml prototype

---
+++ reee_port_01/Common/Handlers.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace reee_port_01
+{
+    public class Note
+    {
+        string notetype;
+        string notecontent;
+        DateTime noterecordtime;
+
+        public string NoteType { get => notetype; set => notetype = value; }
+        public string NoteContent { get => notecontent; set => notecontent = value; }
+        public string NoteRecordtime { get => noterecordtime.ToString("yyyy-MM-dd-HH_mm_ss"); }
+
+        public Note()
+        {
+            noterecordtime = DateTime.Now;
+        }
+
+        public Note(string notetype, string notecontent)
+            : this()
+        {
+            this.notetype = notetype;
+            this.notecontent = notecontent;
+        }
+        
+    }
+
+    public class NoteBug : Note
+    {
+        string str;
+        string ar;
+        string er; 
+        bool regr;
+
+
+    }
+
+    public class NoteScreenshot
+    {
+
+    }
+
+    //public delegate void SubmitHandler(string a, string b, string c);
+
+    public class NoteDataXml
+    {
+        
+        string xmlreportpath = Environment.CurrentDirectory + "//ree_" + DateTime.Now.ToString("yyyy-mm-dd-HH_mm_ss") + ".xml";
+
+        public string XmlReportPath { get => xmlreportpath; }
+
+        public void CreateXmlReport()
+        {
+            XDocument xmlreport =
+                new XDocument(
+                    new XElement("Report")
+                    );
+            xmlreport.Save(xmlreportpath);
+
+        }
+
+        public void WriteToXmlReport(Note note, NoteDataXml xmldocumnet)
+        {
+            XDocument xmlreport = XDocument.Load(xmldocumnet.XmlReportPath);
+            XElement rootelement = new XElement("Note");
+
+            rootelement.Add(new XElement("Time", note.NoteRecordtime));
+            rootelement.Add(new XElement("Type", note.NoteType));
+            rootelement.Add(new XElement("Content", note.NoteContent));
+
+            xmlreport.Element("Report").Add(rootelement);
+            xmlreport.Save(xmldocumnet.XmlReportPath);
+        }
+
+    }
+
+    
+}
---
+++ reee_port_01/Views/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace reee_port_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NoteDataXml sessionree;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void Application_Startup(object sender, EventArgs e)
+        {
+
+            //temporaty - populate combobox with data from txt
+            string[] NoteTypeComboBox = File.ReadAllLines("NoteType.txt");
+
+            NoteType.ItemsSource = NoteTypeComboBox;
+            NoteType.SelectedItem = "Bug";
+
+
+            NoteField.Focus();
+
+
+            NoteDataXml ree = new NoteDataXml();
+            ree.CreateXmlReport();
+
+            sessionree = ree;
+
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NoteFiled_KeyDown(Object sender, KeyEventArgs e)
+        {
+ 
+            if (e.Key == Key.Enter && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+            {
+
+                Note note = new Note(NoteType.Text, NoteField.Text);
+                sessionree.WriteToXmlReport(note, sessionree);            
+
+                NoteField.Clear();
+
+            }       
+
+        }
+
+        private void NoteType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
