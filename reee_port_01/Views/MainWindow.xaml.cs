72db41652e461252e77a62bcfd8400fc36aa1bff
Alexey Zadunaiskiy <alexey.zdn@gmail.com>
2020-02-07 00:11:55 +0200

    Removed local saving from UI

--- reee_port_01/Views/MainWindow.xaml.cs
+++ reee_port_01/Views/MainWindow.xaml.cs
@@ -48,17 +48,6 @@ namespace reee_port_01
 
                 Note note = new Note(NoteType.Text, NoteField.Text);
 
-                if (settings.GenerateXml == true)
-                {
-                    if (!File.Exists(settings.XmlReportPath))
-                    {
-                        Directory.CreateDirectory(Path.Combine(settings.XmlFolder, settings.XmlSubFolder));
-                        xmlHandler.CreateXmlReport(settings.XmlReportPath);
-                    }
-
-                    xmlHandler.WriteToXmlReport(note, settings.XmlReportPath);
-                }
-
                 if (settings.GenarateGoogleSheet == true)
                 {
                     sheetHandler.AppendToSheet(note, settings.SpreadsheetID, settings.SheetRange);
