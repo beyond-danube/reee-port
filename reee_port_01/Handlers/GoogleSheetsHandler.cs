d59f544108859a72924969dab9f2c8a4a65afa26
Alexey Zadunaiskiy <alexey.zdn@gmail.com>
2020-02-08 22:50:33 +0200

    Revert "Removed local XML report saving"
    This reverts commit 0ff5c5bf5bbc08a38e13d97a41978d63d87a985d.

--- reee_port_01/Handlers/GoogleSheetsHandler.cs
+++ reee_port_01/Handlers/GoogleSheetsHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using System.IO;
+using System.Diagnostics;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using System.Threading;
@@ -24,6 +26,8 @@ namespace reee_port_01
 
             using (var stream = new FileStream(@"Resources\credentials.json", FileMode.Open, FileAccess.Read))
             {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
                 string credPath = @"Resources\token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
@@ -31,23 +35,30 @@ namespace reee_port_01
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
+                Debug.WriteLine("Credential file saved to: " + credPath);
             }
 
+            // Create Google Sheets API service.
             var service = new SheetsService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
                 ApplicationName = ApplicationName,
             });
 
+            // The ID of the spreadsheet to update.
+            string spreadsheetId = spreadsheetID;  // TODO: Update placeholder value.
 
-            string spreadsheetId = spreadsheetID;
+            // The A1 notation of a range to search for a logical table of data.
+            // Values will be appended after the last row of the table.
+            string range = sheetRange;  // TODO: Update placeholder value.
 
-            string range = sheetRange;
-
+            // How the input data should be interpreted.
             SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum valueInputOption = (SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW);  // TODO: Update placeholder value.
 
+            // How the input data should be inserted.
             SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum insertDataOption = (SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS);  // TODO: Update placeholder value.
 
+            // TODO: Assign values to desired properties of `requestBody`:
             Data.ValueRange requestBody = new Data.ValueRange();
 
             var arr = new string[] { note.NoteType, note.NoteContent, note.NoteRecordtime};
@@ -58,8 +69,12 @@ namespace reee_port_01
             request.ValueInputOption = valueInputOption;
             request.InsertDataOption = insertDataOption;
 
-            request.Execute();
+            // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+            Data.AppendValuesResponse response = request.Execute();
+            // Data.AppendValuesResponse response = await request.ExecuteAsync();
 
+            // TODO: Change code below to process the `response` object:
+            Debug.WriteLine(JsonConvert.SerializeObject(response));
         }
     }
 }
---
+++ reee_port_01/Handlers/XMLHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace reee_port_01
+{
+
+    public class XMLHandler
+    { 
+
+        public void CreateXmlReport(string xmlPath)
+        {
+            XDocument xmlreport =
+                new XDocument(
+                    new XElement("Report")
+                    );
+            xmlreport.Save(xmlPath);
+
+        }
+
+        public void WriteToXmlReport(Note note, string xmlPath)
+        {
+            XDocument xmlreport = XDocument.Load(xmlPath);
+            XElement rootelement = new XElement("Note");
+
+            rootelement.Add(new XElement("Time", note.NoteRecordtime));
+            rootelement.Add(new XElement("Type", note.NoteType));
+            rootelement.Add(new XElement("Content", note.NoteContent));
+
+            xmlreport.Element("Report").Add(rootelement);
+            xmlreport.Save(xmlPath);
+        }
+
+    }
+}
